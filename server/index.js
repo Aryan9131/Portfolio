4880bd72d718e5848896995ff9eb8ebbe7b34efc
Aryan Nayak <aryannayak9131@gmail.com>
2025-01-04 10:48:27 +0530

    Updaed index.js cors

--- server/index.js
+++ server/index.js
@@ -1,27 +1,26 @@
-const express=require('express');
-const cors=require('cors')
-const app=express();
-const PORT=process.env.port || 8000;
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 8000;
 
 // Enable CORS
 const corsOptions = {
-    origin: [
-      'https://portfolio-frontend-bay-delta.vercel.app'
-    ],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ["Authorization", "Content-Type"],
-    credentials: true
-  };
+    origin: 'https://portfolio-frontend-bay-delta.vercel.app', // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], 
+    allowedHeaders: ['Authorization', 'Content-Type'],
+    credentials: true, // Allow cookies, if needed
+};
 
 app.use(cors(corsOptions));
-app.use(express.urlencoded());
-app.use(express.json())
-app.use('/', require('./routes'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use('/', require('./routes'));
 
-app.listen(PORT, (error)=>{
-    if(error){
-        console.log("Error while running server : "+error);
-    }else{
-        console.log("Server is running at port : "+PORT);
+// Start the server
+app.listen(PORT, (error) => {
+    if (error) {
+        console.log("Error while running server : " + error);
+    } else {
+        console.log("Server is running at port : " + PORT);
     }
-})
+});
