010eaf8461db01362a97e413dbc94834bf303898
Aryan Nayak <aryannayak9131@gmail.com>
2025-01-04 10:19:34 +0530

    Updated Deployed Backend Url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://portfolio-one-virid-79.vercel.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
