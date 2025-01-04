d8a6351ee2f0fe9a90bebad50fdc70dc747abe5c
Aryan Nayak <aryannayak9131@gmail.com>
2025-01-04 12:48:42 +0530

    Allowed origin

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 8000;
 dotenv.config();
 // Enable CORS
 const corsOptions = {
-    origin: ['https://portfolio-frontend-bay-delta.vercel.app'], // Frontend URL
+    origin: '*', // Frontend URL
     methods: ['GET', 'POST', 'PUT', 'DELETE'], 
     allowedHeaders: ['Authorization', 'Content-Type'],
     credentials: true, // Allow cookies, if needed
